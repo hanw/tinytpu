906f337ed3d45dda6c43f59cec01c87e5de25b1b
Han Wang <h1337h4x0r@gmail.com>
2026-04-09 21:47:13 -0700

    tests: cover TinyTPU full-tile add

--- tests/test_tinytpu_backend_gemm.py
+++ tests/test_tinytpu_backend_gemm.py
@@ -134,6 +134,12 @@ class TestTinyTPUBackendGemm(unittest.TestCase):
     ).numpy()
     np.testing.assert_array_equal(result, np.array([4, 7, 6], dtype=np.int32))
 
+  def test_vpu_add_full_tile_matches_reference(self):
+    a_np = np.arange(16, dtype=np.int32)
+    b_np = np.arange(100, 116, dtype=np.int32)
+    result = (Tensor(a_np, dtype="int32", device="TINYTPU") + Tensor(b_np, dtype="int32", device="TINYTPU")).numpy()
+    np.testing.assert_array_equal(result, a_np + b_np)
+
 
 class TestTinyTPUTilingInference(unittest.TestCase):
   def test_infers_single_tile_shape(self):
