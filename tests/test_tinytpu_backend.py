820356f1c68d569f7f4371bbebe91080c3145a34
Han Wang <h1337h4x0r@gmail.com>
2026-04-18 13:39:03 -0700

    tests: add 4x1 row-sum coverage

--- tests/test_tinytpu_backend.py
+++ tests/test_tinytpu_backend.py
@@ -4538,6 +4538,11 @@ class TestTinyTPUBackend(unittest.TestCase):
     result = Tensor(data, dtype="int32", device="TINYTPU").sum(axis=0).numpy()
     np.testing.assert_array_equal(result, data.sum(axis=0))
 
+  def test_rowsum_4x1_matches_reference(self):
+    data = np.array([[1], [2], [3], [4]], dtype=np.int32)
+    result = Tensor(data, dtype="int32", device="TINYTPU").sum(axis=1).numpy()
+    np.testing.assert_array_equal(result, data.sum(axis=1))
+
 
 class TestTinyTPUTilingInference(unittest.TestCase):
   def test_infers_single_tile_shape(self):
